c217c476f8aa009e2f0272a6969037061a835706
noehassiel <artetools@iMac-de-Arte.local>
2022-01-25 16:44:59 -0600

    cartoon form

--- resources/views/layouts/log,blade.php
+++ resources/views/layouts/log,blade.php
@@ -6,15 +6,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Login page</title>
   <link rel="stylesheet" href="./build/css/index.css">
+  <link rel="stylesheet" href="{{ asset('css/log.css') }}">
 </head>
 
 <body>
 
-
+    @yield('log')
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js" integrity="sha256-S6G5lg9rzC1JCAkx3dQFqP2lefkFxwlNVn0rWCOueXA="
     crossorigin="anonymous"></script>
-  <script src="./cartoon.js"></script>
+  <script src="{{ asset('js/cartoon.js') }}"></script>
 </body>
 
 </html>
