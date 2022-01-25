82895bbcf8b7a355e45d276944abdc648099961a
noehassiel <artetools@iMac-de-Arte.local>
2022-01-25 16:24:06 -0600

    first commit

---
+++ resources/views/layouts/log,blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login page</title>
+  <link rel="stylesheet" href="./build/css/index.css">
+</head>
+
+<body>
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js" integrity="sha256-S6G5lg9rzC1JCAkx3dQFqP2lefkFxwlNVn0rWCOueXA="
+    crossorigin="anonymous"></script>
+  <script src="./cartoon.js"></script>
+</body>
+
+</html>
